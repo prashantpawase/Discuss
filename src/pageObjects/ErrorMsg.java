9c04e69a10ec8c9b34818cd7bac293959f9a38f6
PrAsHaNt PaWaSe <pawaseprashant007@gmail.com>
2017-01-19 13:01:23 +0530

    Added my discuss project

--- src/pageObjects/ErrorMsg.java
+++ src/pageObjects/ErrorMsg.java
@@ -19,20 +19,25 @@ public static WebDriver driver = null;
 public static WebElement Qstn(WebDriver driver) throws InterruptedException
 {
 
-        WebElement erromsgsize =  driver.findElement(By.xpath("//span[@class='msg']"));
-        String txt = erromsgsize.getText();
-        
-        
-        
-            if(txt.equals("Question posted successfully"))
-            {
-            	 System.out.println("true");
-            }
-            else
-            {
-        //    	System.out.println("false");
-            	driver.navigate().to("http://www.edunuts.com/discussions");
-            }
+	WebElement erromsg =  driver.findElement(By.xpath("//span[@class='msg']"));
+    String txt = erromsg.getText();
+       
+    
+    
+     if(txt.equals("Question posted ssuccessfully\033[0m"))
+      {
+        System.out.println("\033[32;1;2mtrue");
+        System.out.println("\033[32;1;2mSuccess Message\033[0m ---> " + erromsg.getText());
+
+      }
+      else
+      {
+       
+   	   System.out.println("false");
+       System.out.println("\033[31;1mError Message\033[0m ---> " + erromsg.getText());
+       driver.navigate().to("http://www.edunuts.com/discussions");
+      
+      }
       
 		return null;
 
